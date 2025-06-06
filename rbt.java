88b23c32ca8e1fd333ee75c06cc7c4fd07a27b1d
srinithhhi <nithhii294@gmail.com>
2025-06-06 14:31:10 +0530

    Create rbt.java

---
+++ rbt.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+	    TreeSet <Integer> set = new TreeSet <> ();
+	    set.add(10);
+	    set.add(57);
+	    set.add(2);
+	    System.out.println(set);
+	}
+}
